21ca1d78c725e03088af80c56e6c2940f1c59827
Martin Ficzel <ficzel@sitegeist.de>
2016-07-29 18:10:55 +0200

    TASK: Add endpoints for all sites and for all configured breakpoints
    The resulting default urls currently are:
    - /sitegeist.monocle/api/breakpoints
    - /sitegeist.monocle/api/sites

--- Classes/Sitegeist/Monocle/Controller/ApiController.php
+++ Classes/Sitegeist/Monocle/Controller/ApiController.php
@@ -4,6 +4,8 @@ namespace Sitegeist\Monocle\Controller;
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\Controller\ActionController;
+use TYPO3\Neos\Domain\Repository\SiteRepository;
+use TYPO3\Neos\Domain\Model\Site;
 
 use Sitegeist\Monocle\Helper\TypoScriptHelper;
 use Sitegeist\Monocle\Helper\ContextHelper;
@@ -16,7 +18,6 @@ class ApiController extends ActionController
      * @var array
      */
     protected $defaultViewObjectName = 'TYPO3\Flow\Mvc\View\JsonView';
-
 
     /**
      * @Flow\Inject
@@ -35,6 +36,18 @@ class ApiController extends ActionController
      * @var ContextHelper
      */
     protected $contextHelper;
+
+    /**
+     * @Flow\Inject
+     * @var SiteRepository
+     */
+    protected $siteRepository;
+
+    /**
+     * @var array
+     * @Flow\InjectConfiguration("breakpoints")
+     */
+    protected $breakpointConfiguration;
 
     /**
      * Get all styleguide objects
@@ -59,6 +72,37 @@ class ApiController extends ActionController
                 }
             }
         }
-        $this->view->assign('value',$styleguideObjects);
+        $this->view->assign('value', $styleguideObjects);
+    }
+
+    /**
+     * Get all active sites
+     *
+     * @return void
+     */
+    public function sitesAction() {
+        $siteInfo = [];
+        $sites = $this->siteRepository->findAll()->toArray();
+        /**
+         * @var Site $site
+         */
+        foreach ($sites as $site) {
+            $siteInfo[$site->getNodeName()] = [
+                'name' => $site->getName(),
+                'online' => $site->isOnline(),
+                'nodeName' => $site->getNodeName(),
+                'siteResourcesPackageKe' => $site->getSiteResourcesPackageKey()
+             ];
+        }
+        $this->view->assign('value', $siteInfo);
+    }
+
+    /**
+     * Get all configured breakpoints
+     *
+     * @return void
+     */
+    public function breakpointsAction() {
+        $this->view->assign('value', $this->breakpointConfiguration);
     }
 }
