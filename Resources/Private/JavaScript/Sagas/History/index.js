07fe33e7cd5b1edd7ce80b13252a437c34566402
Martin Ficzel <ficzel@sitegeist.de>
2016-10-20 15:57:54 +0200

    TASK: Adjust history saga to actually work

--- Resources/Private/JavaScript/Sagas/History/index.js
+++ Resources/Private/JavaScript/Sagas/History/index.js
@@ -1,22 +1,21 @@
 import { takeEvery, takeLatest } from 'redux-saga'
-import { call, put } from 'redux-saga/effects'
-import {redux} from '../../Redux/index';
-
-function* pathChanged(action) {
-    switch (action.type) {
-        case redux.actionTypes.BOOT:
-            if (window.location.hash && window.location.hash !== '#') {
-                yield put(redux.Styleguide.actions.setPath(window.location.hash.substring(1)))
-            } else if (action.payload && action.payload.defaultPath) {
-                yield put(redux.Styleguide.actions.setPath(action.payload.defaultPath))
-            }
-        case redux.Styleguide.actionTypes.SET_PATH:
-            yield window.location.hash = action.payload;
-    }
-}
+import { call, put, take } from 'redux-saga/effects'
+import { redux } from '../../Redux/index';
 
 function* historySaga() {
-    yield* takeEvery([redux.actionTypes.BOOT, redux.Styleguide.actionTypes.SET_PATH], pathChanged);
+
+    yield take(redux.actionTypes.BOOT);
+
+    if (window.location.hash && window.location.hash !== '#') {
+        yield put(redux.Styleguide.actions.setPath(window.location.hash.substring(1)));
+    } else if (action.payload && action.payload.defaultPath) {
+        yield put(redux.Styleguide.actions.setPath(action.payload.defaultPath));
+    }
+
+    while (true) {
+        const action = yield take(redux.Styleguide.actionTypes.SET_PATH);
+        window.location.hash = action.payload;
+    }
 }
 
 export default historySaga;
