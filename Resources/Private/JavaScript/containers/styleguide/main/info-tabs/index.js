dec401715e4e3ee5d23afcebee65b94c485550d9
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-05-27 19:05:15 +0200

    FEATURE: Implement info-tabs

--- Resources/Private/JavaScript/containers/styleguide/main/info-tabs/index.js
+++ Resources/Private/JavaScript/containers/styleguide/main/info-tabs/index.js
@@ -1,17 +1,48 @@
 import React, {PureComponent} from 'react';
+import {connect} from 'react-redux';
+import pretty from 'pretty';
 
-import {resizable} from 'components';
+import Tabs from '@neos-project/react-ui-components/lib/Tabs';
 
+import {visibility, resizable} from 'components';
+import {selectors} from 'state';
+
+import Code from './code';
+
+import style from './style.css';
+import tabTheme from './tabTheme.css';
+import tabPanelTheme from './tabPanelTheme.css';
+
+@connect(state => {
+    const currentlyRenderedPrototype = selectors.prototypes.currentlyRendered(state);
+    const currentlySelectedPrototype = selectors.prototypes.currentlySelected(state);
+
+    return {
+        ...currentlyRenderedPrototype,
+        ...currentlySelectedPrototype,
+        isVisible: Boolean(currentlyRenderedPrototype) && Boolean(currentlySelectedPrototype)
+    };
+})
+@visibility
 @resizable({
     initialHeight: 320,
-    collapsedHeight: 40
+    collapsedHeight: 40,
+    toggleHandleClassName: style.toggleHandle
 })
 export default class InfoTabs extends PureComponent {
     render() {
+        const {title, prototypeName, description, renderedHtml, renderedCode, parsedCode} = this.props;
+
         return (
-            <div>
-                INFOTABS
-            </div>
+            <Tabs className={style.infoTabs} theme={tabTheme}>
+                <Tabs.Panel title={title} icon="info" theme={tabPanelTheme}>
+                    <h2>{title} <small className={style.prototypeLabel}>{prototypeName}</small></h2>
+                    {description}
+                </Tabs.Panel>
+                <Tabs.Panel title="HTML" icon="code" theme={tabPanelTheme}><Code content={pretty(renderedHtml)} language="html" /></Tabs.Panel>
+                <Tabs.Panel title="Fusion" icon="terminal" theme={tabPanelTheme}><Code content={renderedCode} language="vim" /></Tabs.Panel>
+                <Tabs.Panel title="Fusion AST" icon="terminal" theme={tabPanelTheme}><Code content={parsedCode} language="yaml" /></Tabs.Panel>
+            </Tabs>
         );
     }
 }
