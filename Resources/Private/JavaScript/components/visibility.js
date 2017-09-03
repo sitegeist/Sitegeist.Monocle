52a66e47f9de54c1e0f863c006e038efcca456cc
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2017-09-03 12:33:23 +0200

    TASK: Add propType validation to visibility HoC

--- Resources/Private/JavaScript/components/visibility.js
+++ Resources/Private/JavaScript/components/visibility.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default Component => ({isVisible, ...props}) => (
-    isVisible ? <Component {...props}/> : null
-);
+export default Component => {
+    const visibility = ({isVisible, ...props}) => (
+        isVisible ? <Component {...props}/> : null
+    );
+
+    visibility.propTypes = {
+        isVisible: PropTypes.bool.isRequired
+    };
+
+    return visibility;
+};
--- Resources/Public/JavaScript/App.js
+++ Resources/Public/JavaScript/App.js
@@ -31002,17 +31002,27 @@ var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = __webpack_require__(115);
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 exports.default = function (Component) {
-    return function (_ref) {
+    var visibility = function visibility(_ref) {
         var isVisible = _ref.isVisible,
             props = _objectWithoutProperties(_ref, ['isVisible']);
 
         return isVisible ? _react2.default.createElement(Component, props) : null;
     };
+
+    visibility.propTypes = {
+        isVisible: _propTypes2.default.bool.isRequired
+    };
+
+    return visibility;
 };
 
 /***/ }),
