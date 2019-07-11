280e0df450892c2244cc07f59a2a58af66c7b471
Martin Ficzel <ficzel@sitegeist.de>
2019-07-11 09:22:28 +0200

    FEATURE: Allow defining of whole props and propSets as Fusion prototypes or expressions
    Defining whole props and propSets as Expressions or Fusion prototypes allows to
    centralize the fixtures and even extract them into separate json files.
    
    When props or propSets cannot be evaluated directly the keys below are evaluated
    as RawArray as before so this change is a non breaking feature.
    
    ```
    prototype (Vendor.Site:Example) < prototype(Neos.Fusion:Component) {
        @styleguide {
            propSets {
    	    fromEelExpression = ${{title: 'hello world'}}
    	    fromFusionPrototype = Vendor.Site:Example.Fixtures
    	    fromJsonFile = ${Json.parse(File.readFile('_path_to_fixctures.json'), true)}	}
        }
    }
    ```

---
+++ Classes/FusionObjects/CanRenderImplementation.php
@@ -0,0 +1,47 @@
+<?php
+namespace Sitegeist\Monocle\FusionObjects;
+
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+/**
+ * CanRender Fusion-Object
+ *
+ * The CanRender Fusion object checks whether the given type can be rendered
+ */
+class CanRenderImplementation extends AbstractFusionObject
+{
+
+    /**
+     * Fusion path which shall be checked
+     *
+     * @return string
+     */
+    public function getRenderPath()
+    {
+        return $this->fusionValue('renderPath');
+    }
+
+    /**
+     * Fusion type which shall be checked
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->fusionValue('type');
+    }
+
+    /**
+     * @return boolean
+     */
+    public function evaluate()
+    {
+        if ($this->getRenderPath()) {
+            return $this->runtime->canRender($this->getRenderPath());
+        } elseif ($this->getType()) {
+            return $this->runtime->canRender('/type<' . $this->getType() . '>');
+        } else {
+            return false;
+        }
+    }
+}
